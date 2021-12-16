f08167e3736c00151a32e6c0540d148e9cde0649
Rinku Samanta <rinkusamanta12@outlook.com>
2021-12-16 21:10:24 +0530

    main changes

--- client/api/build-client.js
+++ client/api/build-client.js
@@ -6,13 +6,13 @@ export default ({ req }) => {
 
     return axios.create({
       baseURL:
-        'http://ingress-nginx-controller.ingress-nginx.svc.cluster.local',
+        'http://www.ankitsamanta.xyz/',
       headers: req.headers,
     });
   } else {
     // We must be on the browser
     return axios.create({
-      baseUrl: 'http://www.ankitsamanta.xyz/',
+      baseUrl: '/',
     });
   }
 };
